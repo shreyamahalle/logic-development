3aff99c12bd23eecad60521e55ba5f89530335df
shreya mahalle <shreyamahalle2000@gmail.com>
2025-05-20 11:16:24 +0530

    emp project added

---
+++ employee-timesheet-task-management-system/src/main/java/com/shreya/timesheet/EmployeeTimesheetTaskManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.shreya.timesheet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeTimesheetTaskManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeTimesheetTaskManagementSystemApplication.class, args);
+	}
+
+}
---
+++ employee-timesheet-task-management-system/src/test/java/com/shreya/timesheet/EmployeeTimesheetTaskManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shreya.timesheet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeTimesheetTaskManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
