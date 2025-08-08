6f74486c570e5b17af4bd007588c167b2d9ed5f8
shreya <shreyamahalle2000@gmail.com>
2025-08-08 17:56:21 +0530

    health care project funtionality added

--- healthcare-management/src/com/shreya/healthcare/HealthCare.java
+++ healthcare-management/src/com/shreya/healthcare/HealthCare.java
@@ -1,6 +1,65 @@
 package com.shreya.healthcare;
-public class HealthCare {
-    public static void main(String[] args) {
 
+import com.shreya.healthcare.model.Patient;
+
+import java.util.Scanner;
+
+public class HealthCare {
+    private static final Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        HealthCare healthCare = new HealthCare();
+        int option = 0;
+        do {
+            System.out.println("----------------Health-Care Management----------------");
+            System.out.println("Please choose the option:");
+            System.out.println("1. Create Patient:");
+            System.out.println("2. Create Department:");
+            System.out.println("3. Create Doctor:");
+            System.out.println("4. Create Appointment:");
+            System.out.println("5. Create Nurse:");
+            System.out.println("6. Create Hospital:");
+            System.out.println("7. Create Billing:");
+            System.out.println("8. Create Address:");
+            System.out.println("9. Create Prescription:");
+            System.out.println("10. Exit Project:");
+            option = Integer.parseInt(scanner.nextLine());
+            if(option == 1){
+               Patient patient = healthCare.createPatient();
+               System.out.println("Patient Created " + patient);
+            }
+        }while (option !=0);
+    }
+
+    private Patient createPatient(){
+        System.out.println("Enter id");
+        int id = Integer.parseInt(scanner.nextLine());
+        System.out.println("Enter fName");
+        String fName = (scanner.nextLine());
+        System.out.println("Enter lName");
+        String lName = scanner.nextLine();
+        System.out.println("Enter mobileNo");
+        String mobileNo = (scanner.nextLine());
+        System.out.println("Enter alternativeMobileNo");
+        String alternativeMobileNo = (scanner.nextLine());
+        System.out.println("Enter address");
+        String  address = (scanner.nextLine());
+        System.out.println("Enter paymentDetails");
+        String paymentDetails = (scanner.nextLine());
+        System.out.println("Enter age");
+        int age = Integer.parseInt(scanner.nextLine());
+        System.out.println("Enter gender");
+        String gender = (scanner.nextLine());
+        Patient patient = new Patient();
+        patient.setId(id);
+        patient.setfName(fName);
+        patient.setlName(lName);;
+        patient.setMobileNo(mobileNo);
+        patient.setAlternativeMobileNo(alternativeMobileNo);
+        patient.setAddress(address);
+        patient.setPaymentDetails(paymentDetails);
+        patient.setAge(age);
+        patient.setGender(gender);
+
+        return patient;
     }
 }
--- healthcare-management/src/com/shreya/healthcare/model/Patient.java
+++ healthcare-management/src/com/shreya/healthcare/model/Patient.java
@@ -1,7 +1,7 @@
 package com.shreya.healthcare.model;
 
 public class Patient {
-    private String id;
+    private int id;
     private String fName;
     private String lName;
     private String mobileNo;
@@ -12,4 +12,90 @@ public class Patient {
     private String gender;
 
 
+    @Override
+    public String toString() {
+        return "Patient{" +
+                "id='" + id + '\'' +
+                ", fName='" + fName + '\'' +
+                ", lName='" + lName + '\'' +
+                ", mobileNo='" + mobileNo + '\'' +
+                ", alternativeMobileNo='" + alternativeMobileNo + '\'' +
+                ", address='" + address + '\'' +
+                ", paymentDetails='" + paymentDetails + '\'' +
+                ", age=" + age +
+                ", gender='" + gender + '\'' +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+
+    public String getMobileNo() {
+        return mobileNo;
+    }
+
+    public void setMobileNo(String mobileNo) {
+        this.mobileNo = mobileNo;
+    }
+
+    public String getAlternativeMobileNo() {
+        return alternativeMobileNo;
+    }
+
+    public void setAlternativeMobileNo(String alternativeMobileNo) {
+        this.alternativeMobileNo = alternativeMobileNo;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPaymentDetails() {
+        return paymentDetails;
+    }
+
+    public void setPaymentDetails(String paymentDetails) {
+        this.paymentDetails = paymentDetails;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
 }
